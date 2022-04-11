b90cda2c5ee9c5bf0bd87bd3e750f616d69b42fb
Marty44 <xmacin02@vutbr.cz>
2022-04-11 22:25:07 +0200

    Add project files.

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Complex_Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Complex_Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Calc()
+        {
+            return View();
+        }
+
+        public ActionResult Plus
+        {
+
+        }
+    }
+}
