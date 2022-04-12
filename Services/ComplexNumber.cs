4d868be04f81df8df7f52a2a71fab69daa8e190e
Marty44 <xmacin02@vutbr.cz>
2022-04-12 20:48:00 +0200

    making operations with buttons and crating service with complex numbers

---
+++ Services/ComplexNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class ComplexNumber
+{
+    private double Real;
+    private double Imaginary;
+    
+
+    public ComplexNumber(double real = 0.0, double imaginary = 0.0)
+    {
+        Real = real;
+        Imaginary = imaginary;
+
+    }
+
+    public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+    }
+    public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+    }
+    public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+    }
+    public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(((a.Real * b.Real + a.Imaginary * b.Imaginary) + (a.Imaginary * b.Real - a.Real * b.Imaginary)) / Math.Pow(b.Real, 2) + Math.Pow(b.Imaginary, 2));
+    }
+    public static bool operator ==(ComplexNumber a, ComplexNumber b)
+    {
+        return (a.Real == b.Real) && (a.Imaginary == b.Imaginary);
+    }
+    public static bool operator !=(ComplexNumber a, ComplexNumber b)
+    {
+        return !(a == b);
+    }
+    public static ComplexNumber operator -(ComplexNumber a)
+    {
+        return new ComplexNumber(-a.Real, -a.Imaginary);
+    }
+
+
+
+    public string Conversion()
+    {
+        double r = Math.Abs(Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2)));
+
+        double fi = Math.Atan2(Imaginary, Real) / Math.PI;
+
+        return string.Format("{0} * e^({1}* \u03C0 * i)", r, fi); ;
+    }
+
+    public double Argument()
+    {
+        return Math.Atan2(Imaginary, Real);
+    }
+
+    public override string ToString()
+    {
+        if (Imaginary >= 0)
+        {
+            return string.Format("{0}+{1}i", Real, Imaginary);
+        }
+        return string.Format("{0}-{1}i", Real, -Imaginary);
+    }
+}
---
+++ Views/Calc/Calculator.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Complex_Calculator.Views.Calc
+{
+    public class Calc : Controller
+    {
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        private ComplexNumber GetComplexNumber(String data)
+        {
+            String[] input = data.Replace("i", "").Split("+");
+            return new ComplexNumber(Convert.ToDouble(input[0]), Convert.ToDouble(input[1]));
+
+        }
+        public ComplexNumber[] GetComplexNumbers()
+        {
+            string number1 = HttpContext.Request.Form["Number1"];
+            string number2 = HttpContext.Request.Form["Number2"];
+            return new []{GetComplexNumber(number1), GetComplexNumber(number2)};
+        }
+
+        public IActionResult Add()
+        {
+            ComplexNumber[] complexNumbers = GetComplexNumbers();
+            ViewBag.output = (complexNumbers[0] + complexNumbers[1]).ToString();
+            ViewBag.number1 = complexNumbers[0].ToString();
+            ViewBag.number2 = complexNumbers[1].ToString();
+            return View("Calculator");
+        }
+
+        public IActionResult Substract()
+        {
+            ComplexNumber[] complexNumbers = GetComplexNumbers();
+            ViewBag.output = (complexNumbers[0] - complexNumbers[1]).ToString();
+            ViewBag.number1 = complexNumbers[0].ToString();
+            ViewBag.number2 = complexNumbers[1].ToString();
+            return View("Calculator");
+        }
+        public IActionResult Multiply()
+        {
+            ComplexNumber[] complexNumbers = GetComplexNumbers();
+            ViewBag.output = (complexNumbers[0] * complexNumbers[1]).ToString();
+            ViewBag.number1 = complexNumbers[0].ToString();
+            ViewBag.number2 = complexNumbers[1].ToString();
+            return View("Calculator");
+        }
+        public IActionResult Divide()
+        {
+            ComplexNumber[] complexNumbers = GetComplexNumbers();
+            ViewBag.output = (complexNumbers[0] / complexNumbers[1]).ToString();
+            ViewBag.number1 = complexNumbers[0].ToString();
+            ViewBag.number2 = complexNumbers[1].ToString();
+            return View("Calculator");
+        }
+
+
+        public IActionResult Conversion()
+        {
+            string number1 = HttpContext.Request.Form["Number1"];
+            if (number1.Length != 0)
+            {
+                ViewBag.output = GetComplexNumber(number1).Conversion();
+            }
+            string number2 = HttpContext.Request.Form["Number2"];
+            if (number2.Length != 0)
+            {
+                ViewBag.output = GetComplexNumber(number2).Conversion();
+            }
+            return View("Calculator");
+        }
+    }
+}
